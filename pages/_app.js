b5f0f01f0fff79bc120f7a0362c5db892701bba1
Theodor Bigu <t.bigu@yahoo.com>
2022-02-19 03:01:02 +0200

    deploy 9

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
-import Image from "next/image";
-import Link from "next/link";
+
 
 function MyApp({ Component, pageProps }) {
   return (
