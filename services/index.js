e22463b5018e05582a629a8eb0ef6d9513eb1f11
Theodor Bigu <t.bigu@yahoo.com>
2022-02-19 12:58:07 +0200

    orderBy for graphql

--- services/index.js
+++ services/index.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export const getPosts = async () => {
   const query = gql`
     query MyQuery {
-      postsConnection {
+      postsConnection(orderBy: createdAt_DESC) {
         edges {
           node {
             author {
