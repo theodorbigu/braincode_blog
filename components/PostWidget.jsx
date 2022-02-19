0defab623d4e2b0b4f8a0e19a67439bb409243d2
Theodor Bigu <t.bigu@yahoo.com>
2022-02-19 02:54:00 +0200

    deploy 7

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import Image from "next/Image";
+import Image from "next/image";
 import moment from "moment";
-import Link from "next/Link";
+import Link from "next/link";
 
 import { getSimilarPosts, getRecentPosts } from "../services";
 
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
-import Image from "next/Image";
-import Link from "next/Link";
+import Image from "next/image";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
   return (
