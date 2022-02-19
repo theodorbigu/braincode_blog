d0da1099f07ef1f5d86059406d3b4561b93b32b6
Theodor Bigu <t.bigu@yahoo.com>
2022-02-19 23:34:58 +0200

    postwidgets

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -28,23 +28,29 @@ const PostWidget = ({ categories, slug }) => {
       {relatedPosts.map((post, index) => (
         <div key={index} className="flex items-center w-full mb-4">
           <div className="w-16 flex-none">
-            <Image
-              alt={post.title}
-              height="60px"
-              width="60px"
-              unoptimized
-              className="align-middle rounded-full"
-              src={post.featuredImage.url}
-            />
+            <Link
+              href={`/post/${post.slug}`}
+              className="cursor-pointer"
+              key={index}
+            >
+              <Image
+                alt={post.title}
+                height="60px"
+                width="60px"
+                unoptimized
+                className="align-middle rounded-full cursor-pointer"
+                src={post.featuredImage.url}
+              />
+            </Link>
           </div>
           <div className="flex-grow ml-4">
+            <p className="text-[#9dadc1] pt-1">
+              <Link href={`/post/${post.slug}`} className="text-md" key={index}>
+                {post.title}
+              </Link>
+            </p>
             <p className="text-text-color text-sm">
               {moment(post.createdAt).format("MMM DD, YYYY")}
-            </p>
-            <p className="text-[#9dadc1] pt-1">
-            <Link href={`/post/${post.slug}`} className="text-md " key={index}>
-              {post.title}
-            </Link>
             </p>
           </div>
         </div>
